cf12a171d5e3c1d457e6454f318b61e86eeb4464
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-12-26 19:18:30 +0100

    refactor(Breadcrumb): remove Panel from stories

--- stories/Breadcrumb.stories.js
+++ stories/Breadcrumb.stories.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { setAddon, storiesOf } from '@storybook/react';
+import { object } from '@storybook/addon-knobs';
+import JSXAddon from 'storybook-addon-jsx';
 import {
   Breadcrumb,
-  Panel,
   HierarchicalMenu,
 } from '../packages/react-instantsearch/dom';
 import { connectHierarchicalMenu } from '../packages/react-instantsearch/connectors';
-import { object } from '@storybook/addon-knobs';
-import { displayName, filterProps, WrapWithHits } from './util';
-import JSXAddon from 'storybook-addon-jsx';
+import { displayName, filterProps, WrapWithHits, WithoutResults } from './util';
 
 setAddon(JSXAddon);
 
@@ -39,6 +38,16 @@ stories
       filterProps,
     }
   )
+  .add('without results', () => (
+    <WrapWithHits hasPlayground={true} linkedStoryGroup="Breadcrumb">
+      <Breadcrumb
+        attributes={['category', 'sub_category', 'sub_sub_category']}
+        renderHeader={() => 'Breadcrumb'}
+      />
+
+      <WithoutResults />
+    </WrapWithHits>
+  ))
   .addWithJSX(
     'with header and footer',
     () => (
@@ -59,9 +68,7 @@ stories
       displayName,
       filterProps,
     }
-  );
-
-stories
+  )
   .add('with custom component', () => (
     <WrapWithHits hasPlayground={true} linkedStoryGroup="Breadcrumb">
       <Breadcrumb
@@ -77,14 +84,12 @@ stories
   ))
   .add('playground', () => (
     <WrapWithHits hasPlayground={true} linkedStoryGroup="Breadcrumb">
-      <Panel title="Category">
-        <Breadcrumb
-          attributes={['category', 'sub_category', 'sub_sub_category']}
-          translations={object('translations', {
-            rootLabel: 'Home',
-          })}
-        />
-      </Panel>
+      <Breadcrumb
+        attributes={['category', 'sub_category', 'sub_sub_category']}
+        translations={object('translations', {
+          rootLabel: 'Home',
+        })}
+      />
       <VirtualHierarchicalMenu
         attributes={['category', 'sub_category', 'sub_sub_category']}
         defaultRefinement="Cooking > Bakeware"
