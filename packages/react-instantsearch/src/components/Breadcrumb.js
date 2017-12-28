a663d6000a65fc5bab3628abb92d71c2657bf1fa
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-12-28 10:23:14 +0100

    refactor(Breadcrumb): convert to functionnal component

--- packages/react-instantsearch/src/components/Breadcrumb.js
+++ packages/react-instantsearch/src/components/Breadcrumb.js
@@ -1,55 +1,34 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
 import translatable from '../core/translatable';
 import Link from './Link';
 
-class Breadcrumb extends Component {
-  static propTypes = {
-    items: PropTypes.arrayOf(
-      PropTypes.shape({
-        label: PropTypes.string.isRequired,
-        value: PropTypes.string.isRequired,
-      })
-    ).isRequired,
-    canRefine: PropTypes.bool.isRequired,
-    cx: PropTypes.func.isRequired,
-    createURL: PropTypes.func.isRequired,
-    refine: PropTypes.func.isRequired,
-    translate: PropTypes.func.isRequired,
-    rootURL: PropTypes.string,
-    renderSeparator: PropTypes.func,
-  };
-
-  static defaultProps = {
-    renderSeparator: () => '>',
-  };
-
-  render() {
-    const {
-      items,
-      canRefine,
-      cx,
-      createURL,
-      refine,
-      translate,
-      rootURL,
-      renderSeparator,
-    } = this.props;
-
-    const rootPath = canRefine ? (
+const Breadcrumb = ({
+  items,
+  canRefine,
+  cx,
+  createURL,
+  refine,
+  translate,
+  rootURL,
+  renderSeparator,
+}) => (
+  <ul className={cx('list')}>
+    {canRefine && (
       <li className={cx('item')}>
         <Link
           className={cx('link')}
-          onClick={() => (!rootURL ? refine() : null)}
+          onClick={() => !rootURL && refine()}
           href={rootURL ? rootURL : createURL()}
         >
           {translate('rootLabel')}
         </Link>
       </li>
-    ) : null;
+    )}
 
-    const breadcrumb = items.map((item, idx) => {
+    {items.map((item, idx) => {
       const isLast = idx === items.length - 1;
+
       return (
         <li className={cx('item', isLast && 'item--selected')} key={idx}>
           <span className={cx('separator')}>{renderSeparator()}</span>
@@ -66,16 +45,29 @@ class Breadcrumb extends Component {
           )}
         </li>
       );
-    });
+    })}
+  </ul>
+);
 
-    return (
-      <ul className={cx('list')}>
-        {rootPath}
-        {breadcrumb}
-      </ul>
-    );
-  }
-}
+Breadcrumb.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  canRefine: PropTypes.bool.isRequired,
+  cx: PropTypes.func.isRequired,
+  createURL: PropTypes.func.isRequired,
+  refine: PropTypes.func.isRequired,
+  translate: PropTypes.func.isRequired,
+  rootURL: PropTypes.string,
+  renderSeparator: PropTypes.func,
+};
+
+Breadcrumb.defaultProps = {
+  renderSeparator: () => '>',
+};
 
 export default translatable({
   rootLabel: 'Home',
