50e3e0f7871c9795667244a22316c1c53a2fe814
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-12-28 12:10:54 +0100

    refactor(Toggle): convert to functionnal component

--- packages/react-instantsearch/src/components/Toggle.js
+++ packages/react-instantsearch/src/components/Toggle.js
@@ -1,33 +1,23 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
 
-class Toggle extends Component {
-  static propTypes = {
-    cx: PropTypes.func.isRequired,
-    currentRefinement: PropTypes.bool.isRequired,
-    refine: PropTypes.func.isRequired,
-    label: PropTypes.string.isRequired,
-  };
+const Toggle = ({ currentRefinement, label, cx, refine }) => (
+  <label className={cx('label')}>
+    <input
+      className={cx('checkbox')}
+      type="checkbox"
+      checked={currentRefinement}
+      onChange={event => refine(event.currentTarget.checked)}
+    />
+    <span className={cx('labelText')}>{label}</span>
+  </label>
+);
 
-  onChange = e => {
-    this.props.refine(e.target.checked);
-  };
-
-  render() {
-    const { cx, currentRefinement, label } = this.props;
-
-    return (
-      <label className={cx('label')}>
-        <input
-          className={cx('checkbox')}
-          type="checkbox"
-          checked={currentRefinement}
-          onChange={this.onChange}
-        />
-        <span className={cx('labelText')}>{label}</span>
-      </label>
-    );
-  }
-}
+Toggle.propTypes = {
+  currentRefinement: PropTypes.bool.isRequired,
+  label: PropTypes.string.isRequired,
+  cx: PropTypes.func.isRequired,
+  refine: PropTypes.func.isRequired,
+};
 
 export default Toggle;
