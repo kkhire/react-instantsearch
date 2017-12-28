6bdc9c3844392bbbfd125bba3db58e2c2ab4d5b8
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-12-28 11:43:13 +0100

    refactor(NumericMenu): convert to functionnal component

--- packages/react-instantsearch/src/components/NumericMenu.js
+++ packages/react-instantsearch/src/components/NumericMenu.js
@@ -1,29 +1,15 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import List from './List';
 import translatable from '../core/translatable';
+import List from './List';
 
-class NumericMenu extends Component {
-  static propTypes = {
-    cx: PropTypes.func.isRequired,
-    items: PropTypes.arrayOf(
-      PropTypes.shape({
-        label: PropTypes.node.isRequired,
-        value: PropTypes.string.isRequired,
-        isRefined: PropTypes.bool.isRequired,
-        noRefinement: PropTypes.bool.isRequired,
-      })
-    ).isRequired,
-    refine: PropTypes.func.isRequired,
-    transformItems: PropTypes.func,
-    canRefine: PropTypes.bool.isRequired,
-    translate: PropTypes.func.isRequired,
-  };
-
-  renderItem = item => {
-    const { cx, refine, translate } = this.props;
-    const label = item.value === '' ? translate('all') : item.label;
-    return (
+const NumericMenu = ({ items, canRefine, cx, refine, translate }) => (
+  <List
+    items={items.map(item => ({ ...item, key: item.value }))}
+    showMore={false}
+    canRefine={canRefine}
+    cx={cx}
+    renderItem={item => (
       <label className={cx('label')}>
         <input
           className={cx('radio')}
@@ -32,25 +18,28 @@ class NumericMenu extends Component {
           disabled={item.noRefinement}
           onChange={() => refine(item.value)}
         />
-        <span className={cx('labelText')}>{label}</span>
+        <span className={cx('labelText')}>
+          {item.value !== '' ? item.label : translate('all')}
+        </span>
       </label>
-    );
-  };
+    )}
+  />
+);
 
-  render() {
-    const { cx, items, canRefine } = this.props;
-
-    return (
-      <List
-        renderItem={this.renderItem}
-        showMore={false}
-        canRefine={canRefine}
-        cx={cx}
-        items={items.map(item => ({ ...item, key: item.value }))}
-      />
-    );
-  }
-}
+NumericMenu.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.node.isRequired,
+      value: PropTypes.string.isRequired,
+      isRefined: PropTypes.bool.isRequired,
+      noRefinement: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+  canRefine: PropTypes.bool.isRequired,
+  cx: PropTypes.func.isRequired,
+  refine: PropTypes.func.isRequired,
+  translate: PropTypes.func.isRequired,
+};
 
 export default translatable({
   all: 'All',
