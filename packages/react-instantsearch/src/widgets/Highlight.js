4c92cfbd337e7d45a29b75182dd0f8b0c3307c90
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-12-28 11:12:10 +0100

    docs(Highlight): use simpler example

--- packages/react-instantsearch/src/widgets/Highlight.js
+++ packages/react-instantsearch/src/widgets/Highlight.js
@@ -22,27 +22,23 @@ const cx = classNames('Highlight');
  * @example
  * import React from 'react';
  *
- * import { connectHits, Highlight, InstantSearch } from 'react-instantsearch/dom';
+ * import { Hits, Highlight, InstantSearch } from 'react-instantsearch/dom';
  *
- * const CustomHits = connectHits(({ hits }) =>
- * <div>
- *   {hits.map(hit =>
- *     <p key={hit.objectID}>
- *       <Highlight attributeName="description" hit={hit} />
- *     </p>
- *   )}
- * </div>
+ * const Hit = ({ hit }) =>
+ *   <p>
+ *     <Highlight attributeName="description" hit={hit} />
+ *   </p>
  * );
  *
  * export default function App() {
  *  return (
  *    <InstantSearch
- *       appId="latency"
- *       apiKey="6be0576ff61c053d5f9a3225e2a90f76"
- *       indexName="ikea"
- *     >
- *       <CustomHits />
- *     </InstantSearch>
+ *      appId="latency"
+ *      apiKey="6be0576ff61c053d5f9a3225e2a90f76"
+ *      indexName="ikea"
+ *    >
+ *      <Hits renderHit={hit => <Hit hit={hit} />} />
+ *    </InstantSearch>
  *  );
  * }
  */
