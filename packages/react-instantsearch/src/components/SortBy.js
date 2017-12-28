e0584243b977e73c89446e6a05135657eba4e851
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-12-28 12:05:07 +0100

    refactor(Sortby): convert to functionnal component

--- packages/react-instantsearch/src/components/SortBy.js
+++ packages/react-instantsearch/src/components/SortBy.js
@@ -1,35 +1,26 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-
 import Select from './Select';
 
-class SortBy extends Component {
-  static propTypes = {
-    cx: PropTypes.func.isRequired,
-    refine: PropTypes.func.isRequired,
+const SortBy = ({ items, currentRefinement, cx, refine }) => (
+  <Select
+    items={items}
+    selectedItem={currentRefinement}
+    cx={cx}
+    onSelect={refine}
+  />
+);
 
-    items: PropTypes.arrayOf(
-      PropTypes.shape({
-        label: PropTypes.string,
-        value: PropTypes.string.isRequired,
-      })
-    ).isRequired,
-
-    currentRefinement: PropTypes.string.isRequired,
-    transformItems: PropTypes.func,
-  };
-
-  render() {
-    const { cx, refine, items, currentRefinement } = this.props;
-    return (
-      <Select
-        cx={cx}
-        selectedItem={currentRefinement}
-        onSelect={refine}
-        items={items}
-      />
-    );
-  }
-}
+SortBy.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      value: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  currentRefinement: PropTypes.string.isRequired,
+  cx: PropTypes.func.isRequired,
+  refine: PropTypes.func.isRequired,
+};
 
 export default SortBy;
