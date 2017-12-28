eb4447ec4034e937f300d9ede4ad09f7a94117e0
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-12-28 10:55:26 +0100

    refactor(HierarchicalMenu): convert to functionnal component

--- packages/react-instantsearch/src/components/HierarchicalMenu.js
+++ packages/react-instantsearch/src/components/HierarchicalMenu.js
@@ -1,38 +1,29 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { pick } from 'lodash';
-
 import translatable from '../core/translatable';
 import List from './List';
 import Link from './Link';
 
-const itemsPropType = PropTypes.arrayOf(
-  PropTypes.shape({
-    label: PropTypes.string.isRequired,
-    value: PropTypes.string,
-    count: PropTypes.number.isRequired,
-    items: (...args) => itemsPropType(...args),
-  })
-);
-
-class HierarchicalMenu extends Component {
-  static propTypes = {
-    cx: PropTypes.func.isRequired,
-    translate: PropTypes.func.isRequired,
-    refine: PropTypes.func.isRequired,
-    createURL: PropTypes.func.isRequired,
-    canRefine: PropTypes.bool.isRequired,
-    items: itemsPropType,
-    showMore: PropTypes.bool,
-    limitMin: PropTypes.number,
-    limitMax: PropTypes.number,
-    transformItems: PropTypes.func,
-  };
-
-  renderItem = item => {
-    const { cx, createURL, refine } = this.props;
-
-    return (
+const HierarchicalMenu = ({
+  items,
+  canRefine,
+  cx,
+  refine,
+  createURL,
+  translate,
+  showMore,
+  limitMin,
+  limitMax,
+}) => (
+  <List
+    items={items}
+    canRefine={canRefine}
+    showMore={showMore}
+    limitMin={limitMin}
+    limitMax={limitMax}
+    cx={cx}
+    translate={translate}
+    renderItem={item => (
       <Link
         className={cx('link')}
         onClick={() => refine(item.value)}
@@ -41,29 +32,30 @@ class HierarchicalMenu extends Component {
         <span className={cx('label')}>{item.label}</span>{' '}
         <span className={cx('count')}>{item.count}</span>
       </Link>
-    );
-  };
+    )}
+  />
+);
 
-  render() {
-    const { cx } = this.props;
+const itemsPropType = PropTypes.arrayOf(
+  PropTypes.shape({
+    label: PropTypes.string.isRequired,
+    count: PropTypes.number.isRequired,
+    value: PropTypes.string,
+    items: (...args) => itemsPropType(...args),
+  })
+);
 
-    return (
-      <List
-        renderItem={this.renderItem}
-        cx={cx}
-        {...pick(this.props, [
-          'translate',
-          'items',
-          'showMore',
-          'limitMin',
-          'limitMax',
-          'isEmpty',
-          'canRefine',
-        ])}
-      />
-    );
-  }
-}
+HierarchicalMenu.propTypes = {
+  items: itemsPropType.isRequired,
+  canRefine: PropTypes.bool.isRequired,
+  cx: PropTypes.func.isRequired,
+  refine: PropTypes.func.isRequired,
+  createURL: PropTypes.func.isRequired,
+  translate: PropTypes.func.isRequired,
+  showMore: PropTypes.bool,
+  limitMin: PropTypes.number,
+  limitMax: PropTypes.number,
+};
 
 export default translatable({
   showMore: extended => (extended ? 'Show less' : 'Show more'),
