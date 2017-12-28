daaeecb27a745ad9114887b384d0060265673d25
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-12-28 10:27:10 +0100

    refactor(ClearRefinements): convert to functionnal component

--- packages/react-instantsearch/src/components/ClearRefinements.js
+++ packages/react-instantsearch/src/components/ClearRefinements.js
@@ -1,31 +1,27 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-
 import translatable from '../core/translatable';
 
-class ClearRefinements extends Component {
-  static propTypes = {
-    cx: PropTypes.func.isRequired,
-    translate: PropTypes.func.isRequired,
-    items: PropTypes.arrayOf(PropTypes.object).isRequired,
-    refine: PropTypes.func.isRequired,
-  };
+const ClearRefinements = ({ items, cx, refine, translate }) => {
+  const isDisabled = items.length === 0;
 
-  render() {
-    const { cx, translate, items, refine } = this.props;
-    const isDisabled = items.length === 0;
+  return (
+    <button
+      className={cx('button', isDisabled && 'button--disabled')}
+      onClick={() => refine(items)}
+      disabled={isDisabled}
+    >
+      {translate('reset')}
+    </button>
+  );
+};
 
-    return (
-      <button
-        className={cx('button', isDisabled && 'button--disabled')}
-        onClick={() => refine(items)}
-        disabled={isDisabled}
-      >
-        {translate('reset')}
-      </button>
-    );
-  }
-}
+ClearRefinements.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object).isRequired,
+  cx: PropTypes.func.isRequired,
+  refine: PropTypes.func.isRequired,
+  translate: PropTypes.func.isRequired,
+};
 
 export default translatable({
   reset: 'Clear all filters',
